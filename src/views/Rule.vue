b8521cb8adfa9aa05e0db33e00079363491be764
YuChenLiao <3199896248@qq.com>
2020-08-18 23:25:48 +0800

    制作切换动画

--- src/views/Rule.vue
+++ src/views/Rule.vue
@@ -6,19 +6,21 @@
       <br>
       你是第9位玩家
     </div>
+    <button v-on:click="slipup"></button>
     <div 
     :style="{
         height: rule.height+'%',
         width: rule.width+'%',
         backgroundColor: rule.backgroundColor,
-        top: rule.top+'%',
         position: rule.position,
+        bottom: rule.bottom+'%',
         fontSize: rule.fontSize+'px',
         color: rule.color,
         borderTopRightRadius: rule.radius+'px',
         borderTopLeftRadius: rule.radius+'px',
         padding: rule.padding+'px',
         boxSizing: rule.boxSizing,
+        overflow: rule.overflow,
       }"
     >
       在游戏中，你需要选出你看到的美食名称；中途允许退出游戏，但退出后只允许查看自己参与部分的答案，这会让你错失认识更多南大美食的机会哦～
@@ -38,16 +40,29 @@ export default {
   data () {
     return {
       rule: {
-        height: 50,
+        height: 20,
         width: 100,
         backgroundColor: '#FFC21C',
-        top: 50,
+        bottom: 0,
         position: 'absolute',
         fontSize: 18,
         color: '#ffffff',
         radius: 30,
         padding: 30,
-        boxSizing: 'border-box'
+        boxSizing: 'border-box',
+        overflow: 'hidden',
+        flag: 0,
+      }
+    }
+  },
+  methods: {
+    slipup (){
+      if(this.rule.flag === 0){
+        this.rule.height = this.rule.height + 40
+        this.rule.flag = 1
+      } else{
+        this.rule.height = this.rule.height - 40
+        this.rule.flag = 0
       }
     }
   }
